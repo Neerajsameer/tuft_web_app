ddde27db912ba25cb790a5ab330cbd1527f2a170
Manohar Gella <manohargella272@gmail.com>
2024-09-10 01:22:12 +0530

    tab error

--- src/components/dashboard/RoomData.tsx
+++ src/components/dashboard/RoomData.tsx
@@ -7,15 +7,40 @@ import { Card } from "../ui/card";
 export default function RoomData() {
   const pathname = usePathname();
   const router = useRouter();
-  // const tab = pathname.split("/")[3];
-
-  const { feed, getRoomFeedData, getRoomChatData } = useAppStore();
+  
+  const { 
+    feed, 
+    getRoomFeedData, 
+    getRoomChatData, 
+    getRoomFilesData, 
+    getRoomMeetingsData, 
+    getRoomPaymentsData, 
+    payments, 
+    meetings, 
+    files, 
+    messages 
+  } = useAppStore();
 
   const [tab, setTab] = useState("feed");
 
   useEffect(() => {
-    if (tab === "feed") getRoomFeedData({ reset: true });
-    // if (tab === "chat") getRoomChatData({ reset: true });
+    switch(tab) {
+      case "feed":
+        getRoomFeedData({ reset: true });
+        break;
+      case "chat":
+        getRoomChatData({ reset: true });
+        break;
+      case "files":
+        getRoomFilesData({ parent_folder_id: null, reset: true });
+        break;
+      case "meetings":
+        getRoomMeetingsData();
+        break;
+      case "payments":
+        getRoomPaymentsData({ reset: true });
+        break;
+    }
   }, [tab]);
 
   return (
@@ -23,8 +48,8 @@ export default function RoomData() {
       <Tabs
         value={tab}
         onValueChange={(value) => {
-          // router.push(pathname.replace(tab, value));
           setTab(value);
+          // Optionally handle routing or other side effects here
         }}
       >
         <TabsList>
@@ -40,6 +65,30 @@ export default function RoomData() {
           feed.map((item) => (
             <Card className="p-4 m-4" key={item.id}>
               <h1>{item.message}</h1>
+            </Card>
+          ))}
+        {tab === "chat" &&
+          messages.map((message) => (
+            <Card className="p-4 m-4" key={message.id}>
+              <h1>{message.content}</h1>
+            </Card>
+          ))}
+        {tab === "files" &&
+          files.map((file) => (
+            <Card className="p-4 m-4" key={file.id}>
+              <h1>{file.name}</h1>
+            </Card>
+          ))}
+        {tab === "meetings" &&
+          meetings.map((meeting) => (
+            <Card className="p-4 m-4" key={meeting.id}>
+              <h1>{meeting.title}</h1>
+            </Card>
+          ))}
+        {tab === "payments" &&
+          payments.map((payment) => (
+            <Card className="p-4 m-4" key={payment.id}>
+              <h1>{payment.payment_detail}</h1>
             </Card>
           ))}
       </div>
